a26459f6b95105ad853ab9d79d272b41bc159040
Jospine <nzi19001@byui.edu>
2025-01-02 21:29:50 +0300

    update .csproj

--- obj/Debug/net8.0/HotelChatbot.AssemblyInfo.cs
+++ obj/Debug/net8.0/HotelChatbot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelChatbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfce59cd1d54a90fd6fb2523aff8342f1bd71d78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa0145e22d8573d7d8b8c71abecec32514a36104")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelChatbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelChatbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
